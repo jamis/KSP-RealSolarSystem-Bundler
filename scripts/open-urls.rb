7db0aa53ed77e7a7c1ec47b9fc847628d51fff9f
Jamis Buck <jamis@37signals.com>
2014-02-17 14:10:27 -0700

    manifest updates

--- scripts/open-urls.rb
+++ scripts/open-urls.rb
@@ -1,7 +1,21 @@
 require 'yaml'
 require 'shellwords'
 
+page_size = (ARGV.first || 10).to_i
+puts "URLs will be opened in sets of #{page_size}"
+
 mods = YAML.load_file("master.yml")
 urls = mods.map { |m| Shellwords.shellescape(m['home']) }
 
-system "open #{urls.join(' ')}"
+(0..urls.length-1).step(page_size) do |n|
+  subset = urls[n,page_size]
+  system "open #{subset.join(' ')}"
+
+  remaining = urls.length - (n + page_size)
+
+  if remaining > 0
+    puts "#{remaining} urls remaining to display..."
+    puts "Press return to continue"
+    gets
+  end
+end
